ffcdfdb5bd0206c4192cb3d3ee541a985e9e26a0
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-10-23 12:56:44 +0100

    Add different algorithms to calculate Fibonacci numbers

---
+++ src/algorithms/Fibonacci.java
@@ -0,0 +1,66 @@
+package algorithms;
+
+public class Fibonacci {
+	private static long startTime, endTime;
+
+	public static void main(String[] args) {
+		int n;
+		StringBuilder sb = new StringBuilder();
+		String separator = " - ";
+
+		n = 50;
+		System.out.println("For n = " + n);
+
+		startTime();
+		System.out.println("Recursive :");
+		for (int i = 0; i < n; i++) {
+			sb.append(fibonacciRecursive(i)).append(separator);
+		}
+		sb.setLength(sb.length() - separator.length());
+		System.out.println(sb.toString());
+		calculateTime();
+		sb = new StringBuilder();
+		System.out.println("Iterative :");
+		startTime();
+		for (int i = 0; i < n; i++) {
+			sb.append(fibonacciIterativeArray(i)).append(separator);
+		}
+		sb.setLength(sb.length() - separator.length());
+		System.out.println(sb.toString());
+		calculateTime();
+	}
+
+	public static void startTime() {
+		startTime = System.nanoTime();
+	}
+
+	public static void calculateTime() {
+		endTime = System.nanoTime();
+		double duration = (double) (endTime - startTime) / 1000000000;
+		System.out.println("Time taken : " + duration + " seconds");
+	}
+
+	public static long fibonacciRecursive(int n) {
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return 1;
+		return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+	}
+
+	public static long fibonacciIterativeArray(int n) {
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return 1;
+		long[] fib = new long[n + 1];
+		fib[0] = 0;
+		fib[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+		return fib[n];
+
+	}
+
+}
