fe8eb866f6c7c0357527908b626040d5fb2eb947
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-12-04 15:31:22 +0000

    Add Stack Implementation

--- src/datastructures/Stack.java
+++ src/datastructures/Stack.java
@@ -1,5 +1,25 @@
 package datastructures;
 
 public class Stack {
-
+	private SinglyLinkedNode top;
+	
+	public void push(Object item){
+		SinglyLinkedNode n = new SinglyLinkedNode(item);
+		n.setNext(top);
+		top = n;
+	}
+	
+	public Object pop(){
+		if(top!=null){
+			Object item = top.getData();
+			top = top.getNext();
+			return item;
+		}
+		return null;
+	}
+	
+	public Object peek(){
+		return top.getData();
+	}
+	
 }
